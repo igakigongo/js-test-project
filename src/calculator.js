174f1668c396372dba5c73e8cbf1d7835bfcb449
Edward Iga Kigongo <igakigongo@gmail.com>
2020-02-12 22:59:48 +0100

    complete subtraction tests

--- src/calculator.js
+++ src/calculator.js
@@ -1,4 +1,7 @@
 const calculator = () => {
+  function guard(...args) {
+    if (args.length <= 1) throw new Error('too few arguments');
+  }
   /**
    * Sums two operands
    * @param {Number} a
@@ -14,14 +17,35 @@ const calculator = () => {
   }
   /**
    * Divides two operands
-   * @param {Numerator} n
-   * @param {Denominator} d
+   * @param {Number} a - the dividend
+   * @param {Number} d - the divisor
+   * @returns {Number} the result (a number)
+   * @throws {Error} error if the the divisor is zero
    */
   function divide(a, b) {
-    
+    if (b === 0) throw new Error('Cannot divide by zero');
+    return a / b;
   }
-  function multiply(a, b) {}
-  function subtract(a, b) {}
+  /**
+   * Multiplies 2 or more operands
+   * @param  {...any} args
+   * @returns {Number} the result
+   * @throws {Error} error if less than 2 args are passed
+   */
+  function multiply(...args) {
+    guard(...args);
+    return args.reduce((a, c) => a * c, 1);
+  }
+  /**
+   * Subtracts 2 or more operands
+   * @param  {...any} args
+   * @returns {Number} the difference
+   * @throw {Error} error if less than 2 args are passed
+   */
+  function subtract(...args) {
+    guard(...args);
+    return args.reduce((a, c) => a - c);
+  }
   return {
     add,
     divide,
--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,8 +1,8 @@
 import calculator from '../src/calculator';
 
-describe('when adding', () => {
-  const calc = calculator();
+const calc = calculator();
 
+describe('addition', () => {
   it('returns zero if no arguments are given', () => {
     expect(calc.add()).toEqual(0);
   });
@@ -28,27 +28,49 @@ describe('when adding', () => {
 });
 });
 
-// describe('when dividing', () => {
-//   const calc = calculator();
+describe('division', () => {
+  it('throws an exception when denominator is zero', () => {
+    expect(() => calc.divide(2, 0)).toThrow();
+  });
 
-//   it('throws an exception when denominator is zero', () => {
-//     expect(calc.divide(2, 0)).throw();
-//   });
-
-//   test.each`
-//     a     | b     | sum
-//     ${0}  | ${0}  | ${0}
-//     ${10} | ${12} | ${22}
-//     ${15} | ${70} | ${85}
-//   `('expect calc.add($a, $b) to return $sum', ({ a, b, sum }) => {
-//   expect(calc.divide(a, b)).toBe(sum);
-// });
-// });
-
-it.skip('multiplies two numbers', () => {
-
+  test.each`
+    a     | b     | result
+    ${10} | ${12} | ${0.833}
+    ${15} | ${70} | ${0.214}
+  `('expect calc.divide($a, $b) to return $result', ({ a, b, result }) => {
+  expect(calc.divide(a, b)).toBeCloseTo(result);
+});
 });
 
-it.skip('subtracts two numbers', () => {
+describe('multiplication', () => {
+  it('throws an error for args less than 2', () => {
+    expect(() => calc.multiply()).toThrow();
+    expect(() => calc.multiply(1)).toThrow('too few arguments');
+  });
 
+  it('should work for multiple args', () => {
+    expect(calc.multiply(0, 3, 4)).toBeCloseTo(0);
+    expect(calc.multiply(2, 3, 4)).toBeCloseTo(24.0);
+    expect(calc.multiply(5, 1, 9)).toBeCloseTo(45.0);
+  });
+
+  test.each`
+    a     | b      | multiple
+    ${33} | ${2.5} | ${82.5}
+    ${45} | ${0.2} | ${9.0}
+  `('expect calc.multiply($a, $b) to return $multiple', ({ a, b, multiple }) => {
+  expect(calc.multiply(a, b)).toBeCloseTo(multiple);
+});
+});
+
+describe('subtraction', () => {
+  it('throws an error for args less than 2', () => {
+    expect(() => calc.subtract(1)).toThrow('too few arguments');
+  });
+
+  it('should work for multiple args', () => {
+    expect(calc.subtract(0, 3, 4)).toEqual(-7);
+    expect(calc.subtract(2, 3, 4)).toEqual(-5);
+    expect(calc.subtract(5, 1, 9)).toEqual(-5);
+  });
 });
